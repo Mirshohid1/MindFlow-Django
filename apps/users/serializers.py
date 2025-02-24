c7dd8440a1e7d33c1f0fed6d4933c065364339b0
l0n3r <mirshohid1214@gmail.com>
2025-02-24 15:14:57 +0500

    feat: add LoginSerializer

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,4 +1,7 @@
+from typing import Dict, Any
+
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import CustomUser
 
 
@@ -19,3 +22,27 @@ class RegisterSerializer(serializers.ModelSerializer):
         validated_data.pop('password_confirm')
         user = CustomUser.objects.create_user(**validated_data)
         return user
+
+
+class LoginSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['username'] = user.username
+        token['email'] = user.email
+        token['id'] = user.id
+
+        return token
+
+    def validate(self, attrs: Dict[str, Any]) -> Dict[str, str]:
+        data = super().validated_data(attrs)
+
+        data.update({
+            'id': self.user.id,
+            'username': self.user.username,
+            'email': self.user.email,
+            'role': self.user.role
+        })
+
+        return data
