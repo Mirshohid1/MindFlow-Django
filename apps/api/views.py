9cbf7910bd6a2a6f25d995e0ab5da0d629d00182
l0n3r <mirshohid1214@gmail.com>
2025-02-25 23:08:21 +0500

    feat: import requirements, models, serializers

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.exceptions import PermissionDenied
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenBlacklistView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from users.models import (
+    CustomUser, UserSkill, UserProfession,
+    SkillType, Skill, ProfessionType, Profession
+)
+from users.serializers import (
+    SkillTypeSerializer, SkillTypeInputSerializer,
+    SkillSerializer, SkillInputSerializer,
+    ProfessionTypeSerializer, ProfessionTypeInputSerializer,
+    ProfessionSerializer, ProfessionInputSerializer,
+    UserSkillSerializer, UserSKillInputSerializer,
+    UserProfessionSerializer, UserProfessionInputSerializer,
+    RegisterSerializer, LoginSerializer, LogoutSerializer
+)
