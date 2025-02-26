8699ef3b13eea17fec5ef3cadd85b8cd824a39cf
l0n3r <mirshohid1214@gmail.com>
2025-02-26 22:42:10 +0500

    feat: update SkillInputSerializer

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -32,9 +32,27 @@ class SkillSerializer(serializers.ModelSerializer):
 
 
 class SkillInputSerializer(serializers.ModelSerializer):
+    skill_type = serializers.PrimaryKeyRelatedField(
+        queryset=SkillType.objects.all()
+    )
+
     class Meta:
         model = Skill
         fields = ('name', 'description', 'skill_type')
+
+    def to_internal_value(self, data):
+        skill_type_data = data.get('skill_type')
+
+        if isinstance(skill_type_data, dict):
+            try:
+                skill_type = SkillType.objects.get(**skill_type_data)
+            except SkillType.DoesNotExist:
+                raise serializers.ValidationError(
+                    {"skill_type": "Skill type not found, provide a valid ID."}
+                )
+            data['skill_type'] = skill_type.id
+
+        return super().to_internal_value(data)
 
 
 ### --- PROFESSION SERIALIZERS --- ###
