6bfd81dd53619bd7260266e3c2b35a085bfc79f1
l0n3r <mirshohid1214@gmail.com>
2025-02-27 00:47:29 +0500

    refactor: add UserPermissionMixin

--- apps/api/mixins.py
+++ apps/api/mixins.py
@@ -5,3 +5,9 @@ class AdminPermissionMixin:
     def check_admin_permissions(self):
         if self.request.user.role != 'admin':
             raise PermissionDenied("You do not have the rights to perform this action.")
+
+
+class UserPermissionMixin:
+    def check_user_permissions(self, instance):
+        if self.request.user != instance.user:
+            raise PermissionDenied("You do not have the rights to perform this action.")
