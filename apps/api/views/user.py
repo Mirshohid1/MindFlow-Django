a20f63ec8fce01fc06fef9b44207e94467e5a13d
l0n3r <mirshohid1214@gmail.com>
2025-03-01 03:08:56 +0500

    feat(api): add ConfirmEmailView

--- apps/api/views/user.py
+++ apps/api/views/user.py
@@ -1,4 +1,8 @@
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from users.models import UserSkill, UserProfession
 from users.serializers import (
@@ -20,3 +24,23 @@ class UserProfessionViewSet(BaseUserViewSet):
     queryset = UserProfession.objects.all()
     OutputSerializer = UserProfessionSerializer
     InputSerializer = UserProfessionInputSerializer
+
+
+class ConfirmEmailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user = request.user
+        confirmation_code = request.data.get("confirmation_code")
+
+        if not confirmation_code:
+            return Response({"detail": "Код подтверждения обязателен."}, status=status.HTTP_400_BAD_REQUEST)
+
+        if user.confirmation_code != confirmation_code:
+            return Response({"detail": "Неверный код подтверждения."}, status=status.HTTP_400_BAD_REQUEST)
+
+        user.is_email_verified = True
+        user.confirmation_code = None  # Удаляем код после подтверждения
+        user.save()
+
+        return Response({"detail": "Email успешно подтвержден!"}, status=status.HTTP_200_OK)
